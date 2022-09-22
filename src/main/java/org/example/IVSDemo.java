20d12c3c3fe91254ea28f627db0e727d8e3dcd36
liyxmse <liyxmse@amazon.com>
2022-09-22 14:21:05 +0800

    add example for ak sk setting

--- src/main/java/org/example/IVSDemo.java
+++ src/main/java/org/example/IVSDemo.java
@@ -1,27 +1,28 @@
 package org.example;
 
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ivs.IvsClient;
 import software.amazon.awssdk.services.ivs.model.CreateChannelRequest;
 import software.amazon.awssdk.services.ivs.model.CreateChannelResponse;
-import software.amazon.awssdk.services.ivs.model.GetChannelRequest;
-import software.amazon.awssdk.services.ivs.model.GetChannelResponse;
-import software.amazon.awssdk.services.ivs.model.GetStreamKeyRequest;
-import software.amazon.awssdk.services.ivs.model.GetStreamKeyResponse;
-import software.amazon.awssdk.services.ivs.model.ListStreamKeysRequest;
-import software.amazon.awssdk.services.ivs.model.ListStreamKeysResponse;
-import software.amazon.awssdk.services.ivs.model.StreamKey;
 
 import java.util.HashMap;
+import java.util.Properties;
 
 /**
  * Hello world for aws ivs jdk!
  *
  */
-public class IVSDemo
-{
-    public static void main( String[] args )
-    {
+public class IVSDemo {
+    public static void main(String[] args) {
+
+        // ak sk 配置
+        Properties p = System.getProperties();
+        p.put("aws.accessKeyId", "AKIAxxxxx");
+        p.put("aws.secretAccessKey", "WiXXLyxxxxxxx");
+        System.setProperties(p);
+        SystemPropertyCredentialsProvider sysPopsProvider = SystemPropertyCredentialsProvider.create();
+
         // 指定接口区域。控制接口在如下地区可用，选取离服务端位置近的即可
         // 俄勒冈 US_WEST_2
         // 弗吉尼亚北 US_EAST_1
@@ -31,9 +32,12 @@ public class IVSDemo
         // 首尔 AP_NORTHEAST_2
         // 孟买 AP_SOUTH_1
         Region region = Region.AP_NORTHEAST_1;
-        
-        // 创建客户端
-        IvsClient ivsClient = IvsClient.builder().region(region).build();
+
+        // 创建客户端，通过显式设置 ak sk
+        IvsClient ivsClient = IvsClient.builder().credentialsProvider(sysPopsProvider).region(region).build();
+
+        // 创建客户端，从~/.aws/credentials获取权限
+        // IvsClient ivsClient = IvsClient.builder().region(region).build();
 
         // 存放标签，按需要设置
         HashMap<String, String> cTags = new HashMap<>();
@@ -43,8 +47,8 @@ public class IVSDemo
         CreateChannelRequest createChannelRequest = CreateChannelRequest
                 .builder()
                 .latencyMode("LOW") // LOW or NORMAL，频道延迟
-                .name("testFromJAVASDK-v3") // 频道名称，自定义
-                .type("STANDARD")   // BASIC or STANDARD，频道类型
+                .name("testFromJAVASDK-v33") // 频道名称，自定义
+                .type("STANDARD") // BASIC or STANDARD，频道类型
                 .tags(cTags)
                 .build();
         CreateChannelResponse resp = ivsClient.createChannel(createChannelRequest);
@@ -53,6 +57,6 @@ public class IVSDemo
         // 推流key
         String streamKey = resp.streamKey().value();
         // 拼接终端推流地址
-        System.out.println("The ingest address is " + "rtmp://"+ ingestEndpoint+"/app/" + streamKey);
+        System.out.println("The ingest address is " + "rtmp://" + ingestEndpoint + "/app/" + streamKey);
     }
 }
